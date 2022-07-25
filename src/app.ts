ec42b16a448dac414fefcbd047c6320cdeefb242
Allchymist <pedrohenry015@gmail.com>
2022-07-25 13:20:02 -0300

    EsLint and Prettier Installed and  Configured

--- src/app.ts
+++ src/app.ts
@@ -8,9 +8,12 @@ const app = express();
 
 app.use(express.json());
 
-const server = https.createServer({
-  cert: path.resolve(__dirname, 'cert.pem'),
-  key: path.resolve(__dirname, 'privkey.pem')
-}, app);
+const server = https.createServer(
+  {
+    cert: path.resolve(__dirname, 'cert.pem'),
+    key: path.resolve(__dirname, 'privkey.pem'),
+  },
+  app,
+);
 
 server.listen(port, () => console.log('Server started in port: ' + port));
