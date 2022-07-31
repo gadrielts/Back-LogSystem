fdf29fc6134fea2798c2137351d63499abfc0fcc
Allchymist <pedrohenry015@gmail.com>
2022-07-31 16:18:34 -0300

    Implement ensureAuth and Controllers in routes

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 
+import authController from '../controllers/authController';
+import ensureAuth from '../middlewares/ensureAuth';
+
 const UsersRouter = Router();
 
-UsersRouter.get('/:id');
-UsersRouter.post('/login');
-UsersRouter.put('/create');
-UsersRouter.patch('/edit/:id');
-UsersRouter.delete('/delete/:id');
+UsersRouter.get('/perfil', ensureAuth, authController.Perfil);
+UsersRouter.post('/login', authController.Login);
+UsersRouter.put('/create', ensureAuth, authController.Register);
+UsersRouter.patch('/edit', ensureAuth, authController.Edit);
+UsersRouter.delete('/delete', ensureAuth, authController.Delete);
 
 export default UsersRouter;
