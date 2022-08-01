5ff68df91e9abd7ec9af088fce9d6fece9554701
Allchymist <pedrohenry015@gmail.com>
2022-07-31 22:19:23 -0300

    update app to run http and https server

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,26 @@
 import express from 'express';
-import https from 'https';
-import fs from 'fs';
+//import https from 'https';
+//import fs from 'fs';
 
+import { connect } from './database';
 import Routes from './routes';
 
 const port = process.env.PORT;
 const app = express();
-
-const options = {
-  cert: fs.readFileSync('./src/ssl/cert.pem'),
-  key: fs.readFileSync('./src/ssl/privkey.pem'),
-};
-
-const server = https.createServer(options, app);
+connect();
 
 app.use(express.json());
 app.use(Routes);
 
-server.listen(port, () => console.log('Server started in port: ' + port));
+/** if you want to use an https server, here is a recommendation.
+ * const options = {
+ *  cert: fs.readFileSync('./src/ssl/cert.pem'),
+ *  key: fs.readFileSync('./src/ssl/privkey.pem'),
+ * };
+ *
+ * const server = https.createServer(options, app);
+ * server.listen(port, () => console.log('Server started in port: ' + port));
+ */
+
+// the default being used is HTTP. If you don't want to use it, just delete it.
+app.listen(port, () => console.log('Server started in port: ' + port));
